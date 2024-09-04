6af34ec8df08ab7efa510019d9c10e834c1dd6e4
Guillaume <guillaume.rot@gmail.com>
2024-09-04 21:59:32 +0200

    feat: initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import { Title } from "@/feature/Title";
 import { HeaderLogo } from "../feature/HeaderLogo";
 
 export default function Home() {
   return (
-    <div className="bg-blue-100 py-16">
-      <HeaderLogo />
+    <div className="flex flex-col gap-4">
+      <div className="h-16" />
+      {/* <HeaderLogo /> */}
+      <Title />
     </div>
+    // <div className="bg-blue-100 py-16">
+    //   <HeaderLogo />
+    //   <Title />
+    // </div>
   );
 }
