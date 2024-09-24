fc98f2e05881a6f75e9e6823b62773d3c874ba7a
Guillaume <guillaume.rot@gmail.com>
2024-09-24 20:49:14 +0200

    feat: padding subtitle

--- src/feature/landing/SubtitleSection.tsx
+++ src/feature/landing/SubtitleSection.tsx
@@ -5,7 +5,7 @@ import { Layout, LayoutDescription, LayoutTitle } from "@/components/Layout";
 export const SubtitleSection = () => {
     return (
       <div>
-          <LayoutDescription className="mx-auto w-full max-w-screen-xl flex flex-row justify-center text-primary-foreground text-center">
+          <LayoutDescription className="mx-auto w-full max-w-screen-xl flex flex-row justify-center text-primary-foreground text-center px-8">
             Tu souhaites mettre tes articles en ligne sans te préoccuper de la création de ton site ?<br/>
             Nous te proposons de créer ta page avec tes articles en seulement quelques minutes !
           </LayoutDescription>
